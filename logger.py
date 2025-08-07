81453698bbff4fb393b6fc0d08992b4b01b5c7db
HYPER~SHUBH <scammerhurii@gmail.com>
2025-08-07 15:22:38 +0530

    logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,10 @@
+import logging
 
+logging.basicConfig(
+    filename="scanner.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log_result(data):
+    logging.info(f"Scan result: {data}")
